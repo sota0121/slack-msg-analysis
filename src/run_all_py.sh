b1ea3c576ffea55363c5d44038b452c354a49d31
sota_masuda <masuda.kyushu@gmail.com>
2020-04-06 13:20:19 +0900

    全スクリプト実行シェルスクリプト(wordcloud by user 用)を修正

--- src/run_all_py.sh
+++ src/run_all_py.sh
@@ -2,14 +2,14 @@
 cd ./d010_data/
 python slack_msg_extraction.py
 cd ../d020_intermediate/
-python group_msg_by_user.py
-python cleaning.py
+python make_msg_mart_table.py
+python cleaning.py messages.csv
 cd ../d030_processing/
-python morphological_analysis.py
-python normalization.py
-python stopword_removal.py
+python morphological_analysis.py messages_cleaned.csv
+python normalization.py messages_cleaned_wakati.csv
+python stopword_removal.py messages_cleaned_wakati_norm.csv
 cd ../d031_features/
-python important_word_extraction.py
+python important_word_extraction.py 0
 cd ../d070_visualization/
-python wordcloud_from_score.py
+python wordcloud_from_score.py important_words_tfidf_by_user.json
 cd ../../
